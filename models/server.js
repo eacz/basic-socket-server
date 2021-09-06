34bc9ccd61cd986bc99c40b715ace03f30271e80
Esteban Canteros <estebanacanteros@gmail.com>
2021-09-05 21:32:57 -0300

    enviroment variables

--- models/server.js
+++ models/server.js
@@ -8,7 +8,7 @@ const Sockets = require('./sockets')
 class Server {
   constructor(){
     this.app = express()
-    this.port = 4000
+    this.port = process.env.PORT || 4000
 
     //http server
     this.server = http.createServer(this.app);
