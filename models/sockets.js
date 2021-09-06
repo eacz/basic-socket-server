0bbb0fd1aa13110f7f33af1a30820bc4c7c9c98a
Esteban Canteros <estebanacanteros@gmail.com>
2021-09-05 21:37:41 -0300

    console log on message-to-server event added

--- models/sockets.js
+++ models/sockets.js
@@ -12,6 +12,7 @@ class Sockets {
     //On connection
     this.io.on('connection', (socket) => {
       socket.on('message-to-server', data => {
+        console.log(data);
         this.io.emit('message-from-server', data)
       })
     });
